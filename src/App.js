0ec0f17293d145951439e7727546a9a734007dde
Muamar Al Farabi <muamaralfarabi@gmail.com>
2021-02-19 17:44:35 +0700

    LOGIN SCREEN DONE

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { Home } from './pages/index.js';
+import { Home, Login, Register } from './pages/index.js';
 import { Switch, Route } from 'react-router-dom';
 
 import path from './routers/index.js';
-
-import './styles/App.css';
 
 const App = _ => {
   return (
     <div className="App">
 
       <Switch>
+        <Route path={path.login}>
+          <Login />
+        </Route>
+        <Route path={path.register}>
+          <Register />
+        </Route>
         <Route path={path.home}>
           <Home />
         </Route>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,4 @@
 // Index for importing and exporting pages, please collect pages file in here
 export { default as Home } from './Home/Home.js';
+export { default as Login } from './Login/Login.js';
+export { default as Register } from './Register/Register.js';
