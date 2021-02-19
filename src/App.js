94ddaff675e66f1f808a7d8320faf887de0dd731
yopikurniawan <yopikprabowo@gmail.com>
2021-02-19 23:15:01 +0700

    done layout HomePage, NavBar, Pagination, and Footer

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { Home } from './pages/index.js';
+import { Detail } from './pages/index.js';
 import { Switch, Route } from 'react-router-dom';
-
 import path from './routers/index.js';
+import { NavBar } from './helpers/layout/index.js';
 
-import './styles/App.css';
 
 const App = _ => {
   return (
-    <div className="App">
-
-      <Switch>
-        <Route path={path.home}>
-          <Home />
-        </Route>
-      </Switch>
-    </div>
+    <>
+      <NavBar/> 
+        <Switch>
+          <Route exact path={path.home}>
+            <Home />
+          </Route>
+          <Route path={path.detail}>
+            <Detail />
+          </Route>
+        </Switch>
+    </>
   );
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,3 @@
 // Index for importing and exporting pages, please collect pages file in here
 export { default as Home } from './Home/Home.js';
+export { default as Detail } from './Detail/Detail.js';
