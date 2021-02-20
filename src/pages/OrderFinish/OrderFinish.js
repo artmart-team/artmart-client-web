ff124465f863a3eec837a90a0efc72772a31f03f
Muamar Al Farabi <muamaralfarabi@gmail.com>
2021-02-20 20:58:17 +0700

    BG Card for components

--- src/pages/OrderFinish/OrderFinish.js
+++ src/pages/OrderFinish/OrderFinish.js
@@ -3,9 +3,22 @@ import React from 'react';
 const OrderFinish = _ => {
   return (
     <div id="OrderFinish" style={{ borderRadius: 8, padding: 32, marginLeft: 64, marginRight: 64 }}>
-      <h3>Thank you for ordering.</h3>
-      <p>Your order is being processed, please wait for artist's response.</p>
+      <h3>Order finished</h3>
+      <p>Here is your invoice.</p>
 
+      <div style={{ height: 'auto', backgroundColor: '#dddddd', borderRadius: 8, marginTop: 32, padding: 32 }}>
+        <div className="row">
+
+          <div className="col-6">
+            TEST
+          </div>
+
+          <div className="col-6">
+            TEST
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 };
