0197c45b011a7eff78d5a0dba2edfc69d6f9ddcf
Muamar Al Farabi <muamaralfarabi@gmail.com>
2021-02-20 20:51:32 +0700

    Refactoring

--- src/pages/OrderProcess/OrderProcess.js
+++ src/pages/OrderProcess/OrderProcess.js
@@ -5,15 +5,13 @@ import TimerWidget from './components/TimerWidget.js';
 
 const OrderProcess = _ => {
   return (
-    <div id="OrderProcess">
-      <div id="ImageDescriptionCard" style={{ borderRadius: 8, padding: 32, marginLeft: 64, marginRight: 64 }}>
-        <h3>Thank you for ordering.</h3>
-        <p>Your order is being processed, please wait for artist's response.</p>
+    <div id="OrderProcess" style={{ borderRadius: 8, padding: 32, marginLeft: 64, marginRight: 64 }}>
+      <h3>Thank you for ordering.</h3>
+      <p>Your order is being processed, please wait for artist's response.</p>
 
-        <TimerWidget rawTime={432000} />
+      <TimerWidget rawTime={432000} />
 
-        <ChatCardContainer />
-      </div>
+      <ChatCardContainer />
     </div>
   );
 };
