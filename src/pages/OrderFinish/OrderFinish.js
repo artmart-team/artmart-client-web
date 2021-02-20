0fb1652f2862491dc22e085a2cdc295b11f207c7
Muamar Al Farabi <muamaralfarabi@gmail.com>
2021-02-20 20:50:38 +0700

    Preparing splitting components

--- src/pages/OrderFinish/OrderFinish.js
+++ src/pages/OrderFinish/OrderFinish.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const OrderFinish = _ => {
-  return(
-    <div id="OrderFinish">
-      <h1>ORDER FINISH</h1>
+  return (
+    <div id="OrderFinish" style={{ borderRadius: 8, padding: 32, marginLeft: 64, marginRight: 64 }}>
+      <h3>Thank you for ordering.</h3>
+      <p>Your order is being processed, please wait for artist's response.</p>
+
     </div>
   );
 };
