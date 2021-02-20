0257e8274363649f6611a4da241a703e02f6e9a5
Muamar Al Farabi <muamaralfarabi@gmail.com>
2021-02-20 22:24:28 +0700

    New Sistem Rating

--- src/pages/OrderFinish/components/ReviewField.js
+++ src/pages/OrderFinish/components/ReviewField.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BsStarFill } from 'react-icons/bs';
 
 const ReviewField = _ => {
+
+  const [rating, setRating] = useState(0);
+
+  const handleRating = e => {
+    setRating(e.target.value);
+  }
+
   return (
     <div id="ReviewField" style={{ height: 'auto', borderRadius: 8, marginTop: 32, marginBottom: 32, }}>
       <div style={{ height: 'auto', backgroundColor: '#dddddd', borderRadius: 8, padding: 32, marginTop: 16 }}>
+
+        <h5 style={{ marginBottom: 16 }}>Are you happy with this service? Give a review!</h5>
+
         <form action="">
-          <div className="d-flex justify-content-between" style={{ marginBottom: 16 }}>
+
+          <label htmlFor="rating"><h6>Rating</h6></label><br />
+          <div className="d-flex">
             <div className="align-self-center">
-              <h5>Are you happy with this service? Give a review!</h5>
+              <input type="range" min="0" max="5.0" step="0.5" value={rating} onChange={e => handleRating(e)} className="slider align-self-center" id="rating" style={{ color: 'blue' }} />
             </div>
             <div>
-
-              <select className="form-select" aria-label="Default select example">
-                <option value="">--rating--</option>
-                <option value="1">Poor</option>
-                <option value="2">Bad</option>
-                <option value="3">Normal</option>
-                <option value="4">Good</option>
-                <option value="5">Excellent</option>
-              </select>
+              <p style={{ marginBottom: 6, marginLeft: 16, fontWeight: 600 }}><BsStarFill style={{ marginBottom: 3, marginRight: 8 }} />{rating}</p>
             </div>
           </div>
+
           <div className="form-floating">
             <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea" style={{ borderRadius: 8, height: 100 }}></textarea>
             <label htmlFor="floatingTextarea">Comments</label>
@@ -28,7 +34,7 @@ const ReviewField = _ => {
           <button className="btn btn-primary w-100 mt-3" style={{ borderRadius: 8 }}>Send</button>
         </form>
       </div>
-    </div>
+    </div >
   );
 };
 
