5942b7474b6c04f7e10dfc56ef185efec16b5a2e
Muamar Al Farabi <muamaralfarabi@gmail.com>
2021-02-21 19:14:09 +0700

    Done Routing

---
+++ src/pages/ArtistEditProfile/ArtistEditProfile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ArtistEditProfile = _ => {
+  return (
+    <div id="ArtistEditProfile">
+      <h1>EDIT</h1>
+    </div>
+  );
+};
+
+export default ArtistEditProfile;
