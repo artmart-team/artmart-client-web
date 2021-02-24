70b4b68e344ddbcbc7e92a3266d9b8c86f26163e
afistapratama12 <pratama.otori.12@gmail.com>
2021-02-25 02:11:27 +0700

    pagination

--- src/components/layout/Pagination.js
+++ src/components/layout/Pagination.js
@@ -33,9 +33,9 @@ const Pagination = _ => {
               <span aria-hidden="true">&laquo;</span>
             </a>
           </li>
-          <li className="page-item" onClick={pageOne}><a className="page-link" href="#">1</a></li>
-          <li className="page-item" onClick={pagetwo}><a className="page-link" href="#">2</a></li>
-          <li className="page-item" onClick={pageThree}><a className="page-link" href="#">3</a></li>
+          <li className="page-item" onClick={pageOne}><a className="page-link" href="#HomeCard">1</a></li>
+          <li className="page-item" onClick={pagetwo}><a className="page-link" href="#HomeCard">2</a></li>
+          <li className="page-item" onClick={pageThree}><a className="page-link" href="#HomeCard">3</a></li>
           <li className="page-item">
             <a className="page-link" href="#" aria-label="Next">
               <span aria-hidden="true">&raquo;</span>
