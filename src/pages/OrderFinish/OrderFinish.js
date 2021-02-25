099923a1ee887debfa4563a7ec1c6d9d44a589fd
Muamar Al Farabi <muamaralfarabi@gmail.com>
2021-02-25 08:51:05 +0700

    almost

--- src/pages/OrderFinish/OrderFinish.js
+++ src/pages/OrderFinish/OrderFinish.js
@@ -22,11 +22,11 @@ const OrderFinish = _ => {
   }, [])
 
   return (
-    <div id="OrderFinish" style={{ borderRadius: 8, padding: 32, marginLeft: 64, marginRight: 64 }}>
+    <div id="OrderFinish" style={{ borderRadius: 8, paddingTop: 32, marginLeft: 32, marginRight: 32 }}>
       <h3>Order finished</h3>
       <p>Here is your invoice.</p>
 
-      <div style={{ height: 'auto', backgroundColor: '#dddddd', borderRadius: 8, marginTop: 32, padding: 32 }}>
+      <div style={{ height: 'auto', borderRadius: 8, marginTop: 32, padding: 32 }}>
         <div className="row">
 
           <div className="col-6">
--- src/pages/OrderFinish/components/Option.js
+++ src/pages/OrderFinish/components/Option.js
@@ -6,7 +6,7 @@ const Option = ({ option }) => {
     <div className="mb-2 mt-2 form-check" style={{paddingLeft: 0}}>
       <div className="d-flex justify-content-between">
         <p style={{ marginBottom: 0 }}>{ option[0] }</p>
-        <p style={{ marginBottom: 0 }}>+ Rp. { option[1] }</p>
+        <p style={{ marginBottom: 0 }}>+ Rp. { option[1]?.toLocaleString('id-ID') }</p>
       </div>
     </div>
   )
